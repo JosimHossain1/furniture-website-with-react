601630add758569c0f751617ce864d1fa3814a2f
Josim Hossain <emon.on321@gmail.com>
2022-06-25 10:55:45 +0600

    Start Furnite website building

---
+++ src/Components/HomeScreen/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+const HomeScreen = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default HomeScreen;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+
+
+
+  return (
+    <div>
+      
+      <h1>Hello Nav</h1>
+
+    </div>
+  );
+};
+
+export default Navbar;
